a9ea6b0212e2d2e31c020cd5a23c5499c90c28ec
Carly Pritchard <cpritch616@gmail.com>
2022-06-09 17:45:09 -0500

    add time comparison

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,6 @@
 var currentDate = moment().format("MMMM Do, YYYY");
 var displayDay = document.getElementById("currentDay");
 displayDay.innerHTML = currentDate;
-var currentHour = moment().format("HH");
 
 // Refresh task descriptions button 
 $("#refreshBtn").click(function (event){
@@ -11,46 +10,58 @@ $("#refreshBtn").click(function (event){
     localStorage.clear();
 });
 
+// Compare time block to current time
+var currentHour = moment().hour();
+for (var i = 9; i < 17; i++) {
+    if (i < currentHour) {
+        document.getElementById(i.toString()).classList.add('past');
+    } else if (i === currentHour) {
+        document.getElementById(i.toString()).classList.add('present');
+    } else if (i > currentHour) {
+        document.getElementById(i.toString()).classList.add('future');
+    }
+}
+
 // Save task description button 
 $(".saveBtn").click(function (event) {
-    var hr9 = document.getElementById("hr-9").value;
-        localStorage.setItem("task9" , hr9);
-    var hr10 = document.getElementById("hr-10").value;
-        localStorage.setItem("task10" , hr10);
-    var hr11 = document.getElementById("hr-11").value;
-        localStorage.setItem("task11" , hr11);
-    var hr12 = document.getElementById("hr-12").value;
-        localStorage.setItem("task12" , hr12);
-    var hr1 = document.getElementById("hr-1").value;
-        localStorage.setItem("task1" , hr1);
-    var hr2 = document.getElementById("hr-2").value;
-        localStorage.setItem("task2" , hr2);
-    var hr3 = document.getElementById("hr-3").value;
-        localStorage.setItem("task3" , hr3);
-    var hr4 = document.getElementById("hr-4").value;
-        localStorage.setItem("task4" , hr4);
-    var hr5 = document.getElementById("hr-5").value;
-        localStorage.setItem("task5" , hr5);
+    var hr9 = document.getElementById('9').value;
+        localStorage.setItem('text9', hr9);
+    var hr10 = document.getElementById('10').value;
+        localStorage.setItem('text10', hr10);
+    var hr11 = document.getElementById('11').value;
+        localStorage.setItem('text11', hr11);
+    var hr12 = document.getElementById('12').value;
+        localStorage.setItem('text12', hr12);
+    var hr13 = document.getElementById('13').value;
+        localStorage.setItem('text13', hr13);
+    var hr14 = document.getElementById('14').value;
+        localStorage.setItem('text14', hr14);
+    var hr15 = document.getElementById('15').value;
+        localStorage.setItem('text15', hr15);
+    var hr16 = document.getElementById('16').value;
+        localStorage.setItem('text16', hr16);
+    var hr17 = document.getElementById('17').value;
+        localStorage.setItem('text17', hr17);
 })
 
 // Place task descriptions from local storage back onto page
-var saved9 = localStorage.getItem('task9');
-    document.getElementById("hr-9").value = saved9;
-var saved10 = localStorage.getItem('task10');
-    document.getElementById("hr-10").value = saved10;
-var saved11 = localStorage.getItem('task11');
-    document.getElementById("hr-11").value = saved11;
-var saved12 = localStorage.getItem('task12');
-    document.getElementById("hr-12").value = saved12;
-var saved1 = localStorage.getItem('task1');
-    document.getElementById("hr-1").value = saved1;
-var saved2 = localStorage.getItem('task2');
-    document.getElementById("hr-2").value = saved2;
-var saved3 = localStorage.getItem('task3');
-    document.getElementById("hr-3").value = saved3;
-var saved4 = localStorage.getItem('task4');
-    document.getElementById("hr-4").value = saved4;
-var saved5 = localStorage.getItem('task5');
-    document.getElementById("hr-5").value = saved5;
+     var saved9 = localStorage.getItem('text9');
+         document.getElementById('9').value = saved9;
+     var saved10 = localStorage.getItem('text10');
+         document.getElementById('10').value = saved10;
+     var saved11 = localStorage.getItem('text11');
+        document.getElementById('11').value = saved11;
+     var saved12 = localStorage.getItem('text12');
+        document.getElementById('12').value = saved12;
+     var saved13 = localStorage.getItem('text13');
+        document.getElementById('13').value = saved13;
+     var saved14 = localStorage.getItem('text14');
+        document.getElementById('14').value = saved14;
+     var saved15 = localStorage.getItem('text15');
+         document.getElementById('15').value = saved15;
+     var saved16 = localStorage.getItem('text16');
+        document.getElementById('16').value = saved16;
+     var saved17 = localStorage.getItem('text17');
+         document.getElementById('17').value = saved17;
+  
 
-// Comparing time slot to current time 
