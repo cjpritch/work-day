8d947ff52d627fce4c815699a18e71f0685c7ba7
Carly Pritchard <cpritch616@gmail.com>
2022-06-09 16:14:22 -0500

    add ability to save to local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,4 +11,46 @@ $("#refreshBtn").click(function (event){
     localStorage.clear();
 });
 
+// Save task description button 
+$(".saveBtn").click(function (event) {
+    var hr9 = document.getElementById("hr-9").value;
+        localStorage.setItem("task9" , hr9);
+    var hr10 = document.getElementById("hr-10").value;
+        localStorage.setItem("task10" , hr10);
+    var hr11 = document.getElementById("hr-11").value;
+        localStorage.setItem("task11" , hr11);
+    var hr12 = document.getElementById("hr-12").value;
+        localStorage.setItem("task12" , hr12);
+    var hr1 = document.getElementById("hr-1").value;
+        localStorage.setItem("task1" , hr1);
+    var hr2 = document.getElementById("hr-2").value;
+        localStorage.setItem("task2" , hr2);
+    var hr3 = document.getElementById("hr-3").value;
+        localStorage.setItem("task3" , hr3);
+    var hr4 = document.getElementById("hr-4").value;
+        localStorage.setItem("task4" , hr4);
+    var hr5 = document.getElementById("hr-5").value;
+        localStorage.setItem("task5" , hr5);
+})
 
+// Place task descriptions from local storage back onto page
+var saved9 = localStorage.getItem('task9');
+    document.getElementById("hr-9").value = saved9;
+var saved10 = localStorage.getItem('task10');
+    document.getElementById("hr-10").value = saved10;
+var saved11 = localStorage.getItem('task11');
+    document.getElementById("hr-11").value = saved11;
+var saved12 = localStorage.getItem('task12');
+    document.getElementById("hr-12").value = saved12;
+var saved1 = localStorage.getItem('task1');
+    document.getElementById("hr-1").value = saved1;
+var saved2 = localStorage.getItem('task2');
+    document.getElementById("hr-2").value = saved2;
+var saved3 = localStorage.getItem('task3');
+    document.getElementById("hr-3").value = saved3;
+var saved4 = localStorage.getItem('task4');
+    document.getElementById("hr-4").value = saved4;
+var saved5 = localStorage.getItem('task5');
+    document.getElementById("hr-5").value = saved5;
+
+// Comparing time slot to current time 
