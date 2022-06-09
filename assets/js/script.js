229fb67329d4005827f0ade97e98a76cdb0d8a70
Carly Pritchard <cpritch616@gmail.com>
2022-06-09 16:13:43 -0500

    add refresh button to clear tasks

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,4 +4,11 @@ var displayDay = document.getElementById("currentDay");
 displayDay.innerHTML = currentDate;
 var currentHour = moment().format("HH");
 
+// Refresh task descriptions button 
+$("#refreshBtn").click(function (event){
+    event.preventDefault;
+    $("textarea").val("");
+    localStorage.clear();
+});
+
 
